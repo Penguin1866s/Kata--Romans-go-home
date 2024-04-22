4a129cd1fa30ceb20658261a27b0aefaa0d993ec
Pengüin1866 <elobjetivopenguin1866@gmail.com>
2024-04-22 10:42:00 +0200

    build(RomanSymbols.java): Add enum type RomanSymbols.java.

---
+++ app/src/main/java/edu/badpals/romans_go_home/RomanSymbols.java
@@ -0,0 +1,20 @@
+package edu.badpals.romans_go_home;
+
+public enum RomanSymbols {
+    I(1),
+    V(5),
+    X(10),
+    L(50),
+    C(100),
+    D(500),
+    M(1000),
+    IV(4),
+    IX(9),
+    XL(40),
+    XC(90),
+    CD(400),
+    CM(900);
+
+    RomanSymbols(int value) {
+    }
+}
